5a154333372a262fdbe34394efc349790776ee39
資管二B_楊承智 <b0966031908@gmail.com>
2025-09-15 16:37:34 +0800

    第四次Commit(垂直排列)

--- app/src/main/java/tw/edu/pu/csim/s1131235/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1131235/lotto/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,8 +34,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Play(modifier: Modifier = Modifier) {
     var lucky = (1..100).random()
-    Text(
-        text = "樂透數字(1-100)為: $lucky",
-        modifier = modifier
-    )
+   Column(modifier = modifier){
+       Text(
+           text = "樂透數字(1-100)為: $lucky",
+       )
+       Button(
+           onClick = { lucky = (1..100).random() }
+       ) {
+           Text("重新產生樂透碼")
+       }
+   }
 }
